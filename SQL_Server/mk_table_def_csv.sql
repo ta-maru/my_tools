9b1b27ed2865adaac8ab09583f4c621e2081f556
ta-maru <ta-maru@users.noreply.github.com>
2016-06-05 09:17:57 +0900

    不足スクリプト追加

---
+++ SQL_Server/mk_table_def_csv.sql
@@ -0,0 +1,25 @@
+-- table_def.csv
+
+SELECT
+  o.name
+  , c.name
+  , c.column_id
+  , tp.name
+-- , o.type_desc
+-- , c.is_nullable
+-- , c.max_length
+-- , c.precision
+-- , c.scale
+FROM
+  sys.all_objects o
+
+INNER JOIN sys.all_columns c ON (c.object_id = o.object_id)
+
+LEFT OUTER JOIN sys.types tp ON (tp.system_type_id = c.system_type_id)
+
+WHERE
+  (oschema_id = 1) AND (tp.name <> 'sysname')
+ORDER BY
+  type_desc,
+  o.name,
+  c.column_id
