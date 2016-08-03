d766356aa5b98fd54d767157ea5ffdaec1127936
ta-maru <ta-maru@users.noreply.github.com>
2016-08-04 03:54:49 +0900

    誤記修正

--- SQL_Server/mk_table_def_csv.sql
+++ SQL_Server/mk_table_def_csv.sql
@@ -18,7 +18,7 @@ INNER JOIN sys.all_columns c ON (c.object_id = o.object_id)
 LEFT OUTER JOIN sys.types tp ON (tp.system_type_id = c.system_type_id)
 
 WHERE
-  (oschema_id = 1) AND (tp.name <> 'sysname')
+  (o.schema_id = 1) AND (tp.name <> 'sysname')
 ORDER BY
   type_desc,
   o.name,
