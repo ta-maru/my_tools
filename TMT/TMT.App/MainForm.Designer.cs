b8340e58223a93bccc929c93e80954800a946401
ta-maru <ta-maru@users.noreply.github.com>
2017-01-22 20:21:00 +0900

    win tool

---
+++ TMT/TMT.App/MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TMT.App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_debug = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_debug
+            // 
+            this.textBox_debug.Location = new System.Drawing.Point(12, 280);
+            this.textBox_debug.Multiline = true;
+            this.textBox_debug.Name = "textBox_debug";
+            this.textBox_debug.Size = new System.Drawing.Size(660, 170);
+            this.textBox_debug.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.textBox_debug);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_debug;
+    }
+}
+
---
+++ TMT/TMT.App/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMT.App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // イベント追加
+            this.AllowDrop = true;
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            var drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            textBox_debug.Text = string.Empty;
+
+            foreach (var r in drags)
+            {
+                var fi = new FileInfo(r);
+
+                var fname = string.Format("{0}.jpg", fi.LastWriteTime.ToString("yyyyMMdd"));
+                var fpath = Path.Combine(fi.Directory.FullName, fname);
+
+                int cnt = 1;
+                while ( File.Exists(fpath))
+                {
+                    cnt++;
+                    fname = string.Format("{0}_{1}.jpg", fi.LastWriteTime.ToString("yyyyMMdd"), cnt);
+                    fpath = Path.Combine(fi.Directory.FullName, fname);
+                }
+
+                fi.CopyTo(fpath);
+
+                textBox_debug.Text += fpath + Environment.NewLine;
+            }
+        }
+    }
+}
